2992f428257b2ee1ec906996d84be93eb36e5612
Atacadoweb <atacadowebonline@gmail.com>
2021-06-29 00:14:27 -0300

    Herança de classes

--- scripts.js
+++ scripts.js
@@ -12,3 +12,16 @@ return `Estou lendo ${this.title}`
  let book = new Book('Algoritmos para viver', 'Brian', 500);
  let otherbook = new Book('bora bora ', 'Britins', 5800);
  console.log(book,otherbook)
+
+
+ class ITbook extends Book{  //passa a herança
+constructor( title, author, pages, technology){
+    super(title,author, pages);
+    this.technology = technology;
+
+}
+
+ }
+
+ let itBook = new ITbook('bora bora ', 'Britins', 800, 'algoritimos');
+ console.log(itBook)
