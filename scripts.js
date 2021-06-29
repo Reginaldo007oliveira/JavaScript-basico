ce44752c569573064e8e565fd4bd3b9727b98881
Atacadoweb <atacadowebonline@gmail.com>
2021-06-28 21:58:55 -0300

    Operadores Lógicos

--- scripts.js
+++ scripts.js
@@ -1,3 +1,53 @@
-var isStudent = true;
-console.log(typeof isStudent) 
+//Operadores aritiméticos ()
+//adição +
+var sumTwoNumbers = 1+20;
+console.log(sumTwoNumbers)
+
+//subtração -
+var subTwoNumbers = 4-3;
+console.log(subTwoNumbers)
+//multiplicação  *
+var multTwoNumbers = 4*5;
+console.log(multTwoNumbers)
+//divisão /
+var divTwoNumbers = 20/4;
+console.log(divTwoNumbers)
+//módulo ou resto de divisão %
+console.log(8%3)
+//incremento ++
+var num = 9;
+console.log(++num)
+//decremento  --
+var num2 = 4;
+console.log(--num2)
+
+//operadores de atribuição
+// = atribuição
+// atribuição de soma
+var x = 2
+var y = 3
+console.log(x+=y) //vale 5
+console.log(x-=y) // vale 2 .. primeiro a soma e depois a subtração 5 - y = 2
+console.log(x*=y) // x vale dois 2x3 = 6
+console.log(x/=y) // resultado de x é 6 e y é 3 resultado é 2
+console.log(x%=y)
+
+//Operadores de comparação
+// ==  compara
+// === compara valor e tipo
+// != diferente de
+// < ou <= meno ou menor e igual
+// > maior ou maior e igual
+
+//Operadores lógicos
+// e = &&
+// ou =||
+// negação = !
+
+
+
+
+
+
+
 
