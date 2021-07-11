0ffe1c3908e986cbbe2a27488d51bef5350bb6b7
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 19:55:26 -0300

    Usando o For com uma variavel global e um array com 5 elementos

---
+++ For.js
@@ -0,0 +1,6 @@
+// Estrutura de repetição
+var colors = ["black", "white", "yellow", "green", "blue"];
+for (var i = 0; i < colors.length; i++) {
+  // length ou comprimento .. poderia ser i < 5
+  console.log(colors[i]);
+}
