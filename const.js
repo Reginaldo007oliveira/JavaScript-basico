753d1bb425b7766bd47373f723722c29cebd2b83
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 16:50:33 -0300

    Const não pode receber valor  depois de declarada

--- const.js
+++ const.js
@@ -1,4 +1,5 @@
 // atribuindo valor a variavel constante
 //const pode recuperar, mas nunca atribuir
+// não posso  pegar sobrenome = maria .. não da para mudar
 const sobrenome = "Conceição";
 console.log(sobrenome);
