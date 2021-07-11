7b267297611160f2917eec9ae27ca5bc1cf4073b
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 16:45:32 -0300

    Criação da variavel global

---
+++ scripstsCriacaoDeVariaveis.js
@@ -0,0 +1,12 @@
+{
+  var age = 32;
+}
+
+console.log(age);
+
+//hoisting
+//var são declaçãoção de escopo global
+// let pode ser atualizada
+//const não pode ser atualizada
+// var sofrem hosting de escopo
+//var e let podem ser declaradas sem inicializar
