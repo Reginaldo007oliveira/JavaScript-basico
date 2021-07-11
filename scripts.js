eddb8eeccac40493ebc2ec39a3007dbd5606864c
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 16:32:27 -0300

    Revisao Javascript Básico

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//Declaração de variáveis
+// Escopo global var
+var myName = "Simara";
+let language = "javascript";
+const pattern = "ECMAScript";
+
+console.log(myName);
+console.log(pattern);
+console.log(language);
