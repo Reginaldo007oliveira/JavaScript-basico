d0f63821e6bd67523dfe4ec6e735d4270e2b7b2d
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 16:59:57 -0300

    Dados dinamicos e tipados Objeto array function

---
+++ dadosDinamicamenteTipados.js
@@ -0,0 +1,19 @@
+//Dados dinamicamente tipados
+var myName = "Simara"; //string
+var age = 32; // tipo number
+var height = 1.58; //number
+
+var isStudent = false; //boolean
+
+//Objeto, array e function
+var person = {
+  name: "Simara",
+  age: 32,
+};
+//objeto array
+var students = ["Simara", "Maria", "Joao"];
+
+//objeto function
+function sum(a, b) {
+  return a + b;
+}
