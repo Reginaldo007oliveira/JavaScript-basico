34c9292d16576dba3514911a11a1ae943929841f
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 20:06:12 -0300

    Estruturas do while

---
+++ EstruturaWhile.js
@@ -0,0 +1,12 @@
+// Estrutura de repetição bloco de código executa depois da verificação
+var colors = ["black", "white", "yellow", "green", "blue"];
+for (var i = 0; i < colors.length; i++) {
+  // length ou comprimento .. poderia ser i < 5
+  console.log(colors[i]);
+}
+var i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++;
+}
