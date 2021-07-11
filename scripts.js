737560531aa504a63cd52115d1b47edcccad936a
Atacadoweb <atacadowebonline@gmail.com>
2021-07-11 16:38:17 -0300

    Criação de variavel em JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 //Declaração de variáveis
 // Escopo global var
 var myName = "Simara";
+var myName2 = "Joana";
 let language = "javascript";
 const pattern = "ECMAScript";
 
 console.log(myName);
 console.log(pattern);
 console.log(language);
+console.log(myName2);
